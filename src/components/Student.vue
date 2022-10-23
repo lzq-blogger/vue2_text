b257b1a21543377a49a6fd69d666ae05ebc9c7f3
lzq-blogger <2035328097@qq.com>
2022-10-23 14:21:39 +0800

    11

--- src/components/Student.vue
+++ src/components/Student.vue
@@ -2,10 +2,13 @@
     <div>
         <h1 id="name">你好{{name}}</h1>
     </div>
+    
 </template>
 <script>
+import sss from '../components/sss.vue'
     export default {
         name:"Student",
+        components:{sss},
         data(){
             return{
                 name:"刘智强"
